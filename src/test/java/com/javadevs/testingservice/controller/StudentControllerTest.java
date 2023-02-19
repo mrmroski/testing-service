8066d25e05b9d0231bc92b5fc4db1888b88d3c99
patryk <patmirowski@gmail.com>
2023-02-19 19:49:11 +0100

    mniejsze zmiany

--- src/test/java/com/javadevs/testingservice/controller/StudentControllerTest.java
+++ src/test/java/com/javadevs/testingservice/controller/StudentControllerTest.java
@@ -1,20 +1,31 @@
 package com.javadevs.testingservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javadevs.testingservice.DatabaseCleaner;
 import com.javadevs.testingservice.TestingServiceApplication;
+import com.javadevs.testingservice.model.Student;
+import com.javadevs.testingservice.model.command.edit.EditStudentCommand;
+import com.javadevs.testingservice.repository.StudentRepository;
 import liquibase.exception.LiquibaseException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TestingServiceApplication.class)
 @AutoConfigureMockMvc
@@ -23,6 +34,10 @@ class StudentControllerTest {
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Autowired
     private MockMvc postman;
@@ -44,4 +59,27 @@ class StudentControllerTest {
 //    }
 
 
+    @Test
+    void itShouldEditStudentPartially() throws Exception {
+        Student student = new Student();
+        student.setName("Tomek");
+        student.setEmail("tom@o2.pl");
+        student.setLastname("Walczak");
+        student.setStartedAt(LocalDate.now());
+
+        Student savedStudent = studentRepository.save(student);
+
+        EditStudentCommand command = new EditStudentCommand();
+        command.setLastname("Anrzejczyk");
+
+        String commandString = mapper.writeValueAsString(command);
+
+        postman.perform(MockMvcRequestBuilders.put("/api/v1/students/" + savedStudent.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(commandString))
+                .andExpect(status().isOk());
+
+        postman.perform(get("/api/v1/students/" + savedStudent.getId()));
+
+    }
 }
