f7c20881db9bdbd7cb239725f28258ac337b8351
afrykka <szymon.afrykanski@gmail.com>
2023-02-27 19:29:49 +0100

    add velocity template

--- src/main/java/com/javadevs/testingservice/service/EmailSenderService.java
+++ src/main/java/com/javadevs/testingservice/service/EmailSenderService.java
@@ -1,12 +1,18 @@
 package com.javadevs.testingservice.service;
 
 import com.javadevs.testingservice.model.Question;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.StringWriter;
 import java.util.Set;
 
 @Service
@@ -24,23 +30,41 @@ public class EmailSenderService {
     @Value("${spring.mail.username}")
     private String from;
 
-    public void sendPreparingMail(String toEmail, Set<Question> questions) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(from);
-        mailMessage.setTo(toEmail);
-        mailMessage.setText(questions.toString());
-        mailMessage.setSubject(subjectTestReady);
+    public void sendPreparingMail(String toEmail, long examId) throws MessagingException {
+        VelocityContext context = new VelocityContext();
+        context.put("message", "Jak tylko będziesz gotowy przyciśnij przycisk 'WYGENERUJ TEST' by otrzymać kolejnego maila z wygenerowanym testem!");
+        context.put("buttonUrl", "http://localhost:8080/api/v1/exams/accept-request/" + examId);
 
-        javaMailSender.send(mailMessage);
+        Template template = new VelocityEngine().getTemplate("src/main/resources/emailTemplate/request.vm");
+        StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(toEmail);
+        helper.setFrom(from);
+        helper.setSubject(subjectTestReady);
+        helper.setText(writer.toString(), true);
+
+        javaMailSender.send(message);
     }
 
-    public void sendStartOfTestMail(String toEmail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(from);
-        mailMessage.setTo(toEmail);
-        mailMessage.setText("Test2");
-        mailMessage.setSubject(subjectTestStart);
+    public void sendStartOfTestMail(String toEmail, Set<Question> questions) throws MessagingException {
+        VelocityContext context = new VelocityContext();
+        context.put("message", questions);
 
-        javaMailSender.send(mailMessage);
+        Template template = new VelocityEngine().getTemplate("src/main/resources/emailTemplate/success.vm");
+        StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(toEmail);
+        helper.setFrom(from);
+        helper.setSubject(subjectTestStart);
+        String emailContent = writer.toString();
+        helper.setText("Twoje pytania to: \n" + emailContent, true);
+
+        javaMailSender.send(message);
     }
 }
